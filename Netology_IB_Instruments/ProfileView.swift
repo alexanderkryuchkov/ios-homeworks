0c99abaf1578af1e4da2a30c91050d1cf56beb05
Alexander Kryuchkov <alkryuchkov@mail.ru>
2022-05-01 18:34:17 +1100

    made points 1-6

---
+++ Netology_IB_Instruments/ProfileView.swift
@@ -0,0 +1,18 @@
+//
+//  ProfileView.swift
+//  Netology_IB_Instruments
+//
+//  Created by Alexander Kryuchkov on 01.05.2022.
+//
+
+import UIKit
+
+class ProfileView: UIView {
+
+    @IBOutlet weak var nameLabel: UILabel!
+    @IBOutlet weak var birthdayLabel: UILabel!
+    @IBOutlet weak var cityLabel: UILabel!
+    @IBOutlet weak var imageLabel: UIImageView!
+    @IBOutlet weak var textLabel: UITextView!
+
+}
