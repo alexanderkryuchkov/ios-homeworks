d4ba98c4b55ea89d7c30bc2f6bb92e8a5d10bae2
Alexander Kryuchkov <alkryuchkov@mail.ru>
2022-05-02 15:36:04 +1100

    final

--- Netology_IB_Instruments/ProfileView.swift
+++ Netology_IB_Instruments/ProfileView.swift
@@ -7,12 +7,42 @@
 
 import UIKit
 
-class ProfileView: UIView {
 
+class ProfileView: UIView {
+    
     @IBOutlet weak var nameLabel: UILabel!
     @IBOutlet weak var birthdayLabel: UILabel!
     @IBOutlet weak var cityLabel: UILabel!
     @IBOutlet weak var imageLabel: UIImageView!
     @IBOutlet weak var textLabel: UITextView!
 
+    
+    var view: UIView!
+    var nibName: String = "ProfileView"
+
+
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        setup()
+    }
+
+    required init?(coder aDecoder: NSCoder) {
+        super.init(coder: aDecoder)
+        setup()
+    }
+
+    func loadFromNib() -> UIView {
+        let bundle = Bundle(for:  type(of: self))
+        let nib = UINib(nibName: nibName, bundle: bundle)
+        return nib.instantiate(withOwner: self, options: nil).first! as! UIView
+    }
+
+    func setup() {
+        view = loadFromNib()
+        view.frame = bounds
+        view.autoresizingMask = [.flexibleWidth, .flexibleHeight]
+
+        addSubview(view)
+    }
+    
 }
